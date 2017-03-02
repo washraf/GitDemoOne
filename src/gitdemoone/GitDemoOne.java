eeda576b5d6f40d04a05949b573f6c0609d25d0d
Walid <Walid@Start>
2017-03-02 15:08:13 +0200

    I added the main file

--- src/gitdemoone/GitDemoOne.java
+++ src/gitdemoone/GitDemoOne.java
@@ -15,8 +15,8 @@ public class GitDemoOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello Git");
+        System.out.println("Hello GitHub"); 
+        
         
     }
     
