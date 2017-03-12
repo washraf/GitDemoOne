fb235d518652052d646e3b529b0df9764afcbf9b
Walid <Walid@Start>
2017-03-12 13:32:57 +0200

    shleved

--- src/gitdemoone/GitDemoOne.java
+++ src/gitdemoone/GitDemoOne.java
@@ -15,6 +15,7 @@ public class GitDemoOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("This will be stashed");
         System.out.println("Hello Section 112"); 
         System.out.println("Good Bye"); 
         System.out.println("Hello Test"); 
